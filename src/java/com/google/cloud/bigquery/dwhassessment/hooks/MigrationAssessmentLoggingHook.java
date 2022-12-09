f21fca79af480aea45d3dcea658c6fc3a2fedc9a
Roman Fursov <rfursov@google.com>
2022-12-09 12:32:48 +0100

    Integrate EventLogger to a Hook
    This finishes the backbone implementation of the logging hook. The hook logs Avro messages to GCS (as it was tested on Dataproc. It should work the same with HDFS, but it's for further verification) file in a date partitioned folder. The implementation of rollover is not yet tested, but it was taken as is from the HiveProtoLoggingHook implementation.
    
    Verified on Dataproc with Hive v2.3.6 and v3.

--- src/java/com/google/cloud/bigquery/dwhassessment/hooks/MigrationAssessmentLoggingHook.java
+++ src/java/com/google/cloud/bigquery/dwhassessment/hooks/MigrationAssessmentLoggingHook.java
@@ -15,23 +15,9 @@
  */
 package com.google.cloud.bigquery.dwhassessment.hooks;
 
-import static org.apache.hadoop.hive.ql.hooks.Entity.Type.PARTITION;
-import static org.apache.hadoop.hive.ql.hooks.Entity.Type.TABLE;
-
-import com.google.cloud.bigquery.dwhassessment.hooks.avro.AvroSchemaLoader;
-import com.google.cloud.bigquery.dwhassessment.hooks.logger.ExecutionMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import org.apache.avro.Schema;
+import com.google.cloud.bigquery.dwhassessment.hooks.logger.EventLogger;
+import com.google.cloud.bigquery.dwhassessment.hooks.utils.SystemClock;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.generic.GenericRecordBuilder;
-import org.apache.hadoop.hive.conf.HiveConf;
-import org.apache.hadoop.hive.ql.QueryPlan;
-import org.apache.hadoop.hive.ql.exec.Utilities;
-import org.apache.hadoop.hive.ql.exec.mr.ExecDriver;
-import org.apache.hadoop.hive.ql.exec.tez.TezTask;
-import org.apache.hadoop.hive.ql.hooks.Entity;
 import org.apache.hadoop.hive.ql.hooks.ExecuteWithHookContext;
 import org.apache.hadoop.hive.ql.hooks.HookContext;
 import org.slf4j.Logger;
@@ -42,87 +28,12 @@ public class MigrationAssessmentLoggingHook implements ExecuteWithHookContext {
 
   private static final Logger LOG = LoggerFactory.getLogger(MigrationAssessmentLoggingHook.class);
 
-  private static final Schema QUERY_EVENT_SCHEMA = AvroSchemaLoader.loadSchema("QueryEvents.avsc");
-
-  // Just for the example. Real hook will write it to the file
-  public List<GenericRecord> records = new ArrayList<>();
-
   public void run(HookContext hookContext) throws Exception {
-    processQueryEventAvro(hookContext);
-  }
-
-  private void processQueryEventAvro(HookContext hookContext) {
-    QueryPlan plan = hookContext.getQueryPlan();
-
-    LOG.info("Received hook notification for: {}", plan.getQueryId());
-
-    // Make a copy so that we do not modify hookContext conf.
-    HiveConf conf = new HiveConf(hookContext.getConf());
-    List<ExecDriver> mrTasks = Utilities.getMRTasks(plan.getRootTasks());
-    List<TezTask> tezTasks = Utilities.getTezTasks(plan.getRootTasks());
-    ExecutionMode executionMode = getExecutionMode(mrTasks, tezTasks);
-
-    GenericRecord queryEvent =
-        new GenericRecordBuilder(QUERY_EVENT_SCHEMA)
-            .set("QueryId", plan.getQueryId())
-            .set("QueryText", plan.getQueryStr())
-            .set("EventType", hookContext.getHookType().name())
-            .set("Timestamp", plan.getQueryStartTime())
-            .set("User", getUser(hookContext))
-            .set("RequestUser", getRequestUser(hookContext))
-            .set("ExecutionMode", executionMode.name())
-            .set("Queue", getQueueName(executionMode, conf))
-            .set("TablesRead", getTablesFromEntitySet(plan.getInputs()))
-            .set("TablesWritten", getTablesFromEntitySet(plan.getOutputs()))
-            .build();
-    records.add(queryEvent);
-    LOG.info("Processed record: {}", queryEvent);
-  }
-
-  private List<String> getTablesFromEntitySet(Set<? extends Entity> entities) {
-    List<String> tableNames = new ArrayList<>();
-    for (Entity entity : entities) {
-      if (entity.getType() == TABLE || entity.getType() == PARTITION) {
-        tableNames.add(entity.getTable().getCompleteName());
-      }
-    }
-    return tableNames;
-  }
-
-  private String getUser(HookContext hookContext) {
-    return hookContext.getUgi().getShortUserName();
-  }
-
-  private String getRequestUser(HookContext hookContext) {
-    String requestUser = hookContext.getUserName();
-    return requestUser == null ? hookContext.getUgi().getUserName() : requestUser;
-  }
-
-  private ExecutionMode getExecutionMode(List<ExecDriver> mrTasks, List<TezTask> tezTasks) {
-    if (tezTasks.size() > 0) {
-      // Need to go in and check if any of the tasks is running in LLAP mode.
-      for (TezTask tezTask : tezTasks) {
-        if (tezTask.getWork().getLlapMode()) {
-          return ExecutionMode.LLAP;
-        }
-      }
-      return ExecutionMode.TEZ;
-    }
-
-    return mrTasks.size() > 0 ? ExecutionMode.MR : ExecutionMode.NONE;
-  }
-
-  private String getQueueName(ExecutionMode mode, HiveConf conf) {
-    switch (mode) {
-      case LLAP:
-        return conf.get(HiveConf.ConfVars.LLAP_DAEMON_QUEUE_NAME.varname);
-      case MR:
-        return "TODO_MAPREDUCE";
-      case TEZ:
-        return "TODO_TEZ";
-      case NONE:
-      default:
-        return null;
+    try {
+      EventLogger logger = EventLogger.getInstance(hookContext.getConf(), new SystemClock());
+      logger.handle(hookContext);
+    } catch (Exception e) {
+      LOG.error("Got exceptoin while processing event: ", e);
     }
   }
 }
