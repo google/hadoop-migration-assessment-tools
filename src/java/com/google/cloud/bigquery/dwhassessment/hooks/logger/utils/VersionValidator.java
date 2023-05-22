c00b33398ee343152f2b66a8c13b89802775e43d
Roman Fursov <rfursov@google.com>
2023-05-22 12:20:20 +0200

    Report hook incompatibility for versions outside 2.2.x-3.x.x range (#31)
    Report hook incompatibility for versions outside 2.2.x-3.x.x range

---
+++ src/java/com/google/cloud/bigquery/dwhassessment/hooks/logger/utils/VersionValidator.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022-2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.bigquery.dwhassessment.hooks.logger.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.hive.common.util.HiveVersionInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Component for Hive version retrieval and validation. */
+public class VersionValidator {
+  private static final Logger LOG = LoggerFactory.getLogger(VersionValidator.class);
+
+  private static final Pattern pattern = Pattern.compile("^(\\d+)\\.(\\d+).*");
+
+  /**
+   * Returns full Hive version string, in {@code major.minor.patch-tags} format.
+   *
+   * <p>Returning short version string is not possible as {@link HiveVersionInfo#getShortVersion()}
+   * is unreliable. For 2.2.0 package it is set to 2.1.0 – <a
+   * href="http://go/gh/apache/hive/blob/rel/release-2.2.0/pom.xml#L64">tag source</a>.
+   */
+  public static String getHiveVersion() {
+    return HiveVersionInfo.getVersion();
+  }
+
+  /** Validates is Hive version is within supported versions range */
+  public static boolean isHiveVersionSupported(String version) {
+    Matcher matcher = pattern.matcher(version);
+    if (!matcher.matches()) {
+      LOG.warn("Can not parse Hive version '{}', skipping validation", version);
+      return false;
+    }
+
+    int major = Integer.parseInt(matcher.group(1));
+    int minor = Integer.parseInt(matcher.group(2));
+
+    // Covers v2.2.x-v3.x.x
+    boolean isVersionValid = major == 2 ? minor >= 2 : major == 3;
+
+    if (!isVersionValid) {
+      LOG.error(
+          "Current Hive version '{}' is not supported by the Assessment logging hook, logging"
+              + " disabled. Please refer to the documentation.",
+          version);
+    }
+
+    return isVersionValid;
+  }
+
+  private VersionValidator() {}
+}
---
+++ src/javatests/com/google/cloud/bigquery/dwhassessment/hooks/logger/utils/VersionValidatorTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022-2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.bigquery.dwhassessment.hooks.logger.utils;
+
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class VersionValidatorTest {
+
+  @Test
+  public void isHiveVersionSupported_confirmsSupportedVersions() {
+    assertThat(VersionValidator.isHiveVersionSupported("2.2.0")).isTrue();
+    assertThat(VersionValidator.isHiveVersionSupported("2.3.8")).isTrue();
+    assertThat(VersionValidator.isHiveVersionSupported("2.3")).isTrue();
+    assertThat(VersionValidator.isHiveVersionSupported("3.1.3")).isTrue();
+  }
+
+  @Test
+  public void isHiveVersionSupported_rejectsUnsupportedVersions() {
+    assertThat(VersionValidator.isHiveVersionSupported("2.1.9")).isFalse();
+    assertThat(VersionValidator.isHiveVersionSupported("1.1.2")).isFalse();
+    assertThat(VersionValidator.isHiveVersionSupported("4.0.0")).isFalse();
+  }
+
+  @Test
+  public void isHiveVersionSupported_rejectsInvalidVersions() {
+    assertThat(VersionValidator.isHiveVersionSupported("unexpected")).isFalse();
+    assertThat(VersionValidator.isHiveVersionSupported("5")).isFalse();
+    assertThat(VersionValidator.isHiveVersionSupported("3.abc1")).isFalse();
+  }
+
+  @Test
+  public void getHiveVersion_success() {
+    assertThat(VersionValidator.getHiveVersion()).isEqualTo("2.2.0");
+  }
+}
